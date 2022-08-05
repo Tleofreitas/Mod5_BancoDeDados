1dc5768b726c85a3f3127165df9a085e22b1dcbb
Thiago Freitas <thiagoleonardo1995@live.com>
2022-08-05 04:40:23 -0300

    050820 1

---
+++ 06/DBEmpresa.sql
@@ -0,0 +1,86 @@
+CREATE TABLE tb_cargo(
+    id  INT PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    nivel VARCHAR(10),
+    salario FLOAT
+);
+
+CREATE TABLE tb_departamento (
+    id INT PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    sigla VARCHAR (10)
+);
+
+CREATE TABLE tb_funcionario(
+    id INT,
+    nome VARCHAR(30) NOT NULL,
+    sexo CHAR(1),
+    cod_cargo INT NOT NULL,
+    cod_dpto INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cod_cargo) REFERENCES tb_cargo(id),
+    FOREIGN KEY (cod_dpto) REFERENCES tb_departamento(id)
+);
+
+-- DROP TABLE tb_funcionario; -- DELETAR TABELA
+-- DROP DATABASE + "NOME" -- DELETAR BANCO DE DADOS
+
+ALTER TABLE tb_funcionario ADD data_nasc DATE;
+ALTER TABLE tb_funcionario ADD contact_email VARCHAR(10) NOT NULL;
+-- ALTER TABLE tb_funcionario DROP COLUMN data_nasc; -- DELETAR COLUNA
+ALTER TABLE tb_funcionario DROP COLUMN email;
+ALTER TABLE tb_funcionario RENAME contact_email TO email; -- Alterar nome da coluna (RENAME COLUMN também funciona)
+ALTER TABLE tb_funcionario ALTER COLUMN email TYPE VARCHAR(20); -- COLUMN é opcional
+
+-- CRIANDO TABELA SEM PRIMARY KEY ---
+CREATE TABLE table_exemplo (
+    id INT,
+    nome VARCHAR(40)
+);
+
+ALTER TABLE table_exemplo ADD PRIMARY KEY(id); -- Adiciona PK
+-- ALTER TABLE table_exemplo DROP CONSTRAINT table_exemplo_pkey; -- Remover PK
+-------------------------------------
+
+-- Remover chave estrangeira ---
+ALTER TABLE tb_funcionario DROP CONSTRAINT tb_funcionario_cod_cargo_fkey;
+-- Inserir chave estrangeira ---
+ALTER TABLE tb_funcionario ADD FOREIGN KEY (cod_cargo) REFERENCES tb_cargo(id);
+
+-- Inserir registros na tabela ---
+-- tb_cargo ---
+INSERT INTO tb_cargo(id, nome, nivel, salario) VALUES(1, 'Analista de Sistemas', 'JR', 3000.0);
+INSERT INTO tb_cargo(id, nome, nivel, salario) VALUES(2, 'Desenvolvedor', 'JR', 2500.0);
+INSERT INTO tb_cargo(id, nome, nivel, salario) VALUES(3, 'Desenvolvedor', 'Pleno', 5200.0);
+INSERT INTO tb_cargo(id, nome, nivel, salario) VALUES(4, 'Atendente', NULL, 1212.0);
+INSERT INTO tb_cargo(id, nome, nivel, salario) VALUES(5, 'Contador', NULL, 5000.0);
+
+-- tb_departamento ---
+INSERT INTO tb_departamento(id, nome, sigla) VALUES(1, 'Informatica', 'INF');
+INSERT INTO tb_departamento(id, nome, sigla) VALUES(2, 'Financeiro', 'FIN');
+INSERT INTO tb_departamento(id, nome, sigla) VALUES(3, 'Recursos Humanos', 'RH');
+
+-- tb_funcionarios ---
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(1, 'Pedro Nogueira', '2021-03-12', 'M', 1, 1);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(2, 'Maria Silva', '2022-04-22', 'F', 2, 1);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(3, 'Rafaela Silveira', '2022-07-05', 'F', 2, 1);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(4, 'Ricardo Oliveira', '2019-04-06', 'M', 5, 2);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(5, 'Tamara Junqueira', '2015-11-01', 'F', 3, 1);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(6, 'Janete Rosa', '2021-10-01', 'F', 4, 3);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(7, 'Fernando Silva', '2022-04-02', 'M', 3, 1);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(8, 'Rosana Vieira', '2018-08-07', 'F', 2, 1);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(9, 'Leandro Chaves', '2015-05-13', 'M', 5, 2);
+INSERT INTO tb_funcionario(id, nome, data_nasc, sexo, cod_cargo, cod_dpto) VALUES(10, 'João Marques', '2021-03-15', 'M', 3, 1);
+
+-- Editar registros (NÃO esquecer WHERE) ---
+UPDATE tb_funcionario SET nome = 'Ricardo Fernandes Oliveira' WHERE id = 4;
+UPDATE tb_funcionario SET cod_cargo = 3 WHERE id = 3;
+
+-- Deletar registros da tabela (NÃO esquecer WHERE) ---
+DELETE FROM tb_funcionario WHERE id = 10;
+
+
+
+
+
+
